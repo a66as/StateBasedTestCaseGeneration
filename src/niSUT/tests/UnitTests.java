30434cafa8a137840e72dd36403d95834a902233
Abbas Khan <muhammad.abbas@mdh.se>
2024-06-24 15:54:54 +0200

    Added not instrumented SUT

---
+++ src/niSUT/tests/UnitTests.java
@@ -0,0 +1,19 @@
+package niSUT.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import SUT.ThreePlayerGame;
+
+public class UnitTests {
+
+	ThreePlayerGame sut;
+	//example test case that tests server tracking for server p1
+	@Test
+	public void testServer1() {
+	sut= new ThreePlayerGame();
+	sut.p1_Start(); 
+	assertEquals(1,sut.server); 
+	}
+}
